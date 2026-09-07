1b33f561a029d1c91f8f139c8a5694c6cefb7029
Ironboy <ironboy@users.noreply.github.com>
2026-09-07 13:43:06 +0200

    state in objects and shared change handler forms

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,8 +2,31 @@ import { useState } from 'react';
 
 export default function Form() {
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  // Use ONE object with one property per input in the form
+  // as our single state variable, rather than a new state
+  // variable fro each input
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: ''
+  });
+
+  // This function works as a onChange handler
+  // for all input elements in the form
+  function changeData(event) {
+    // Destructure name and value from event.currentTarget
+    // (currentTarget is the HTML INput element that has
+    // the onChange handler)
+    const { name, value } = event.currentTarget;
+    // Update form data
+    // create a new object as a copy of the old (using spread)
+    // and then chenge the correct property
+    setFormData({ ...formData, [name]: value });
+  }
+
+  // Destructure all properties from formData
+  // so that we don't have to write formData.firstName etc
+  // in the value fields
+  const { firstName, lastName } = formData;
 
   return <>
     <h3>{firstName} {lastName}</h3>
@@ -14,7 +37,7 @@ export default function Form() {
           type="text"
           name="firstName"
           value={firstName}
-          onChange={e => setFirstName(e.currentTarget.value)}
+          onChange={changeData}
           required
         />
       </label>
@@ -24,7 +47,7 @@ export default function Form() {
           type="text"
           name="lastName"
           value={lastName}
-          onChange={e => setLastName(e.currentTarget.value)}
+          onChange={changeData}
           required
         />
       </label>
