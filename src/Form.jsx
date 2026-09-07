6b1d74d32d1c19e245e5de046bc5339f50e9dc1a
Ironboy <ironboy@users.noreply.github.com>
2026-09-07 13:29:12 +0200

    labels and some basic styling

--- src/Form.jsx
+++ src/Form.jsx
@@ -8,20 +8,26 @@ export default function Form() {
   return <>
     <h3>{firstName} {lastName}</h3>
     <form>
-      <input
-        type="text"
-        name="firstName"
-        value={firstName}
-        onChange={e => setFirstName(e.currentTarget.value)}
-        required
-      />
-      <input
-        type="text"
-        name="lastName"
-        value={lastName}
-        onChange={e => setLastName(e.currentTarget.value)}
-        required
-      />
+      <label>
+        <span>Förnamn:&nbsp;</span>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={e => setFirstName(e.currentTarget.value)}
+          required
+        />
+      </label>
+      <label>
+        <span>Efternamn:&nbsp;</span>
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={e => setLastName(e.currentTarget.value)}
+          required
+        />
+      </label>
     </form>
   </>;
 }
