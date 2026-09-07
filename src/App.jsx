71f77949f4f846a5e0d1dce733a37923ff3cfaa5
Ironboy <ironboy@users.noreply.github.com>
2026-09-07 14:56:59 +0200

    hook and descriptor as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import Form from './Form';
 
 export default function App() {
-  return <Form />;
+
+  const formDescriptor = {
+    firstName: {
+      label: 'Förnamn',
+      type: 'text',
+      initialValue: '',
+      required: true
+    },
+    lastName: {
+      label: 'Efternamn',
+      type: 'text',
+      initialValue: '',
+      required: true
+    },
+    email: {
+      label: 'E-post',
+      type: 'email',
+      initialValue: '',
+      required: true
+    }
+  };
+
+  return <Form descriptor={formDescriptor} />;
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,21 +1,6 @@
 import useFormDescriptor from "./utils/useFormDescriptor";
 
-export default function Form() {
-
-  const descriptor = {
-    firstName: {
-      label: 'Förnamn',
-      type: 'text',
-      initialValue: 'Jane',
-      required: true
-    },
-    lastName: {
-      label: 'Efternamn',
-      type: 'text',
-      initialValue: 'Doe',
-      required: true
-    }
-  };
+export default function Form({ descriptor }) {
 
   const fields = useFormDescriptor(descriptor);
 
