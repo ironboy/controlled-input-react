2eedfdf8dae8b28b5c91096c21b8509e74b0487b
Ironboy <ironboy@users.noreply.github.com>
2026-09-07 14:51:51 +0200

    custom hook for form descriptions

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,56 +1,27 @@
-import { useState } from 'react';
+import useFormDescriptor from "./utils/useFormDescriptor";
 
 export default function Form() {
 
-  // Use ONE object with one property per input in the form
-  // as our single state variable, rather than a new state
-  // variable fro each input
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: ''
-  });
+  const descriptor = {
+    firstName: {
+      label: 'Förnamn',
+      type: 'text',
+      initialValue: 'Jane',
+      required: true
+    },
+    lastName: {
+      label: 'Efternamn',
+      type: 'text',
+      initialValue: 'Doe',
+      required: true
+    }
+  };
 
-  // This function works as a onChange handler
-  // for all input elements in the form
-  function changeData(event) {
-    // Destructure name and value from event.currentTarget
-    // (currentTarget is the HTML INput element that has
-    // the onChange handler)
-    const { name, value } = event.currentTarget;
-    // Update form data
-    // create a new object as a copy of the old (using spread)
-    // and then chenge the correct property
-    setFormData({ ...formData, [name]: value });
-  }
-
-  // Destructure all properties from formData
-  // so that we don't have to write formData.firstName etc
-  // in the value fields
-  const { firstName, lastName } = formData;
+  const fields = useFormDescriptor(descriptor);
 
   return <>
-    <h3>{firstName} {lastName}</h3>
     <form>
-      <label>
-        <span>Förnamn:&nbsp;</span>
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          onChange={changeData}
-          required
-        />
-      </label>
-      <label>
-        <span>Efternamn:&nbsp;</span>
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          onChange={changeData}
-          required
-        />
-      </label>
+      {fields}
     </form>
   </>;
 }
---
+++ src/utils/useFormDescriptor.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+export default function useFormDescriptor(descriptor) {
+
+  // This function works as a onChange handler
+  // for all input elements in the form
+  function changeData(event) {
+    // Destructure name and value from event.currentTarget
+    // (currentTarget is the HTML INput element that has
+    // the onChange handler)
+    const { name, value } = event.currentTarget;
+    // Update form data
+    // create a new object as a copy of the old (using spread)
+    // and then chenge the correct property
+    setFormData({ ...formData, [name]: value });
+  }
+
+  // Use ONE object with one property per input in the form
+  // as our single state variable, rather than a new state
+  // variable fro each input
+  const [formData, setFormData] = useState(null);
+
+  const fields = [];
+  const initialFormState = {};
+  for (let [formFieldName, fieldDescription] of Object.entries(descriptor)) {
+    const { label, type, initialValue, required } = fieldDescription;
+    const inputAttributes = {
+      name: formFieldName,
+      type,
+      required,
+      onChange: changeData,
+      value: formData ? formData[formFieldName] : initialValue
+    };
+    fields.push(<label key={formFieldName}>
+      <span>{label}:&nbsp;</span>
+      <input {...inputAttributes} />
+    </label>);
+    initialFormState[formFieldName] = initialValue;
+  }
+
+  // If formData is null set it to initialFormState
+  formData === null && setFormData(initialFormState);
+
+  return fields;
+}
